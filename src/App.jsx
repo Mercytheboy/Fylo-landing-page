f9f364b79ff810b0999bc8b2e2511cb8c144a94d
Mercy Dare <daremercy1@gmail.com>
2023-04-05 19:26:44 +0100

    changed intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import Intro from "./containers/intro/Intro";
 import Features from "./containers/features/Features";
 import Work from "./containers/work/Work";
 import Testimonials from "./containers/testimonials/Testimonials";
 import Footer from "./containers/footer/Footer";
+import Intro from "./containers/intro/Intro";
 
 function App() {
   return (
